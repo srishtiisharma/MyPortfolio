1640ed44eed4adb0a0fa07ad88fea134cc5be201
srishtiisharma <brazil_jcb@hotmail.com>
2024-12-10 19:59:07 +0000

    Updates

---
+++ static/js/main.3c61fb78.js
@@ -0,0 +1,2 @@
+(()=>{function e(){document.getElementById("download-link").click()}document.addEventListener("DOMContentLoaded",(function(){var t=document.getElementById("download-button");t&&t.addEventListener("click",e),document.querySelectorAll("nav ul li a").forEach((function(e){e.addEventListener("click",(function(e){e.preventDefault();var t=this.getAttribute("href");window.location.href=t}))}))}))})();
+//# sourceMappingURL=main.3c61fb78.js.map
